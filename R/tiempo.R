bd2c2bf7561416d6d6a01215fc9d2fc4a3adeb38
luciairp <luciarp@gmail.com>
2025-10-21 09:51:50 -0300

    agregué función

---
+++ R/tiempo.R
@@ -0,0 +1,15 @@
+what_time <- function(language = "es") {
+
+  if (!language %in% c("es", "en")) {
+    stop("Either choose 'es' or 'en' as a language.")
+  }
+
+  time <- format(Sys.time(), "%H:%M")
+
+  switch(
+    language,
+    es = sprintf("¡Son las %s!", time),
+    en = sprintf("It is %s now!", time)
+  )
+
+}
