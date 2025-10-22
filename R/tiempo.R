81a7f938778f2417b474217be0691fd22c98b688
luciairp <luciarp@gmail.com>
2025-10-22 09:43:38 -0300

    actualizo información de función what_time

--- R/tiempo.R
+++ R/tiempo.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param language
+#'
+#' @returns cadena de caracteres con la hora
+#' @export
+#'
+#' @examples
+#' what_time()
+#' what_time("en")
 what_time <- function(language = "es") {
 
   if (!language %in% c("es", "en")) {
